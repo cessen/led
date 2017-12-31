e47c0e04512c846b482e12aae9358a6138d3d050
Nathan Vegdahl <cessen@cessen.com>
2017-12-31 03:33:43 -0800

    Updated to the latest versions of dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-// #![feature(test)]
-
-// extern crate test;
 extern crate docopt;
-extern crate encoding;
 extern crate ropey;
 extern crate rustbox;
-extern crate rustc_serialize;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate unicode_segmentation;
 extern crate unicode_width;
-// extern crate freetype;
-// extern crate sdl2;
 
 use std::path::Path;
 use docopt::Docopt;
 use editor::Editor;
 use term_ui::TermUI;
 use term_ui::formatter::ConsoleLineFormatter;
-// use gui::GUI;
-// use gui::formatter::GUILineFormatter;
 
 mod string_utils;
 mod utils;
@@ -25,8 +19,6 @@ mod buffer;
 mod formatter;
 mod editor;
 mod term_ui;
-// mod font;
-// mod gui;
 
 // Usage documentation string
 static USAGE: &'static str = "
@@ -34,12 +26,11 @@ Usage: led [options] [<file>]
        led --help
 
 Options:
-    -g, --gui   Use a graphical user interface instead of a console UI
     -h, --help  Show this message
 ";
 
 // Struct for storing command-line arguments
-#[derive(RustcDecodable, Debug)]
+#[derive(Debug, Deserialize)]
 struct Args {
     arg_file: Option<String>,
     flag_gui: bool,
@@ -49,34 +40,17 @@ struct Args {
 fn main() {
     // Get command-line arguments
     let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.decode())
+        .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
-    // Initialize and start UI
-    if args.flag_gui {
-        // // Load file, if specified
-        // let editor = if let Option::Some(s) = args.arg_file {
-        //     Editor::new_from_file(GUILineFormatter::new(4), &Path::new(&s[..]))
-        // }
-        // else {
-        //     Editor::new(GUILineFormatter::new(4))
-        // };
-        //
-        // // GUI
-        // sdl2::init(sdl2::INIT_VIDEO);
-        // let mut ui = GUI::new_from_editor(editor);
-        // ui.main_ui_loop();
-        // sdl2::quit();
+    // Load file, if specified
+    let editor = if let Option::Some(s) = args.arg_file {
+        Editor::new_from_file(ConsoleLineFormatter::new(4), &Path::new(&s[..]))
     } else {
-        // Load file, if specified
-        let editor = if let Option::Some(s) = args.arg_file {
-            Editor::new_from_file(ConsoleLineFormatter::new(4), &Path::new(&s[..]))
-        } else {
-            Editor::new(ConsoleLineFormatter::new(4))
-        };
+        Editor::new(ConsoleLineFormatter::new(4))
+    };
 
-        // Console UI
-        let mut ui = TermUI::new_from_editor(editor);
-        ui.main_ui_loop();
-    }
+    // Initialize and start UI
+    let mut ui = TermUI::new_from_editor(editor);
+    ui.main_ui_loop();
 }
