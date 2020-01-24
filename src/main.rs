a867892183a06f0c26a686cd5af5d354a178e22c
Nathan Vegdahl <cessen@cessen.com>
2020-01-24 18:32:42 +0900

    Replaced docopt with clap for command line argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-extern crate docopt;
-extern crate ropey;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate smallvec;
-extern crate termion;
-extern crate unicode_segmentation;
-extern crate unicode_width;
-
-use docopt::Docopt;
+use clap::{App, Arg};
 use editor::Editor;
 use std::path::Path;
 use term_ui::formatter::ConsoleLineFormatter;
@@ -21,31 +11,24 @@ mod string_utils;
 mod term_ui;
 mod utils;
 
-// Usage documentation string
-static USAGE: &'static str = "
-Usage: led [options] [<file>]
-       led --help
-
-Options:
-    -h, --help  Show this message
-";
-
-// Struct for storing command-line arguments
-#[derive(Debug, Deserialize)]
-struct Args {
-    arg_file: Option<String>,
-    flag_help: bool,
-}
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    // Get command-line arguments
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.deserialize())
-        .unwrap_or_else(|e| e.exit());
+    // Parse command line arguments.
+    let args = App::new("Led")
+        .version(VERSION)
+        .about("A text editor")
+        .arg(
+            Arg::with_name("file")
+                .help("Text file to open")
+                .required(false)
+                .index(1),
+        )
+        .get_matches();
 
     // Load file, if specified
-    let editor = if let Option::Some(s) = args.arg_file {
-        Editor::new_from_file(ConsoleLineFormatter::new(4), &Path::new(&s[..]))
+    let editor = if let Some(filepath) = args.value_of("file") {
+        Editor::new_from_file(ConsoleLineFormatter::new(4), &Path::new(&filepath[..]))
     } else {
         Editor::new(ConsoleLineFormatter::new(4))
     };
